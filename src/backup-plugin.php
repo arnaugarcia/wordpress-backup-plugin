532b3007fa1eb42962768550b142ee322d56ae69
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2023-01-07 17:49:39 +0100

    Updated plugin name

--- src/backup-plugin.php
+++ src/backup-plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: Backup Plugin
+Plugin Name: Auto Backup plugin
 Plugin URI: http://github.com/arnaugarcia/wordpress-backup-plugin
 Description: This plugin automatically backups the current database and commits the code to the desired repo
 Version: 1.0
